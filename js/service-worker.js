55066c823a733457e6944796229b42ec86190dff
Petr Fedyaev <pefedyaev@ozon.ru>
2025-07-25 10:02:46 +0300

    добавляем манифест и фавайкон

---
+++ js/service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'health-diary-v1';
+const urlsToCache = [
+    '/',
+    'index.html',
+    'manifest.json',
+    'icons/android-icon-192x192.png',
+    'icons/android-icon-512x512.png'
+];
+
+self.addEventListener('install', event => {event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
