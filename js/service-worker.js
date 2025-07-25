27429d02134109cfbe393aee5adc3937b0e2f314
Petr Fedyaev <pefedyaev@ozon.ru>
2025-07-25 10:26:45 +0300

    добавляем манифест и фавайкон

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,17 +1,33 @@
-const CACHE_NAME = 'health-diary-v1';
+// service-worker.js
+const CACHE_NAME = 'health-diary-cache-v1';
 const urlsToCache = [
-    '/',
-    'index.html',
-    'manifest.json',
-    'icons/android-icon-192x192.png',
-    'icons/android-icon-512x512.png'
+    '/health-diary/',
+    '/health-diary/index.html',
+    '/health-diary/manifest.json',
+    '/health-diary/icons/android-icon-192x192.png',
+    '/health-diary/icons/android-icon-512x512.png',
+    '/health-diary/favicon.ico',
+    '/health-diary/style.css',
 ];
 
-self.addEventListener('install', event => {event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))});
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+            .catch(error => {
+                console.error('Cache addAll error:', error);
+            })
+    );
+});
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
